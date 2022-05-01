ade843aa3d7c8959ef666ab6af721903ec422132
jidorta <ibandorta@gmail.com>
2022-05-01 20:33:43 +0200

    inicial

---
+++ backendAlumnos/src/main/java/com/ibandorta/BackendAlumnosApplication.java
@@ -0,0 +1,13 @@
+package com.ibandorta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAlumnosApplication.class, args);
+	}
+
+}
---
+++ backendAlumnos/src/test/java/com/ibandorta/BackendAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibandorta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
